9fb19e710461624f35b4f656030eafaa7d41c4c9
Joshua Stiefer <facedelajunk@gmail.com>
2016-04-01 19:35:00 -0700

    Add screenlayout xrandr script for desktop

---
+++ screenlayout/.screenlayout/Desktop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-1 --mode 1920x1080 --pos 3360x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 1440x0 --rotate normal --output DP-1 --off --output DVI-D-1 --mode 1440x900 --pos 0x0 --rotate normal
